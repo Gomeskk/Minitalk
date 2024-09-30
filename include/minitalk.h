e1340922f0e5fd6d2958a8c1c9d0eec68dc09e8c
Gomeskk <gomesjoao08@hotmail.com>
2024-09-30 14:52:40 +0100

    updates on minitalk.h

---
+++ include/minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joafaust <joafaust@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 14:42:22 by joafaust          #+#    #+#             */
+/*   Updated: 2024/09/30 14:49:12 by joafaust         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
+# include "../libft/libft.h"
+
+#endif
