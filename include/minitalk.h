f716b31aace10d1f70d8d14b3e653e6324a89a52
Gomeskk <gomesjoao08@hotmail.com>
2024-10-03 11:25:23 +0100

    client done

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: joafaust <joafaust@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 14:42:22 by joafaust          #+#    #+#             */
-/*   Updated: 2024/09/30 14:49:12 by joafaust         ###   ########.fr       */
+/*   Updated: 2024/10/03 11:21:53 by joafaust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,12 @@
 # include <signal.h>
 # include <stdlib.h>
 # include "../libft/libft.h"
+
+int		ft_atoi(const char *str);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putendl_fd(char *s, int fd);
+
+# define WAIT_TIME 100
 
 #endif
